43fbd19ad5236e480415b0e2b1c63624e2872301
Vikram0812 <kr.vikram008@gmail.com>
2025-02-02 17:31:58 +0530

    1st commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export async function signup(req,res) {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+export async function login(req,res) {
+    res.send("Login Route");
+}
+
+
+export async function logout(req,res) {
+    res.send("Logout Route");
+}
+
