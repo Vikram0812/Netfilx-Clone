102996fad4609a7740ea7bed4a827cac5e7706bc
Vikram0812 <kr.vikram008@gmail.com>
2025-02-16 19:24:56 +0530

    Built search routes

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,15 @@ import cookieParser from 'cookie-parser';
 import authRoutes from "./routes/auth.route.js"
 import movieRoutes from "./routes/movie.route.js"
 import tvRoutes from "./routes/tv.route.js"
+import searchRoutes from "./routes/search.route.js";
+
+
 
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
-import { protectRoute } from './middleware/protectRoute.js';
+import { protectRoute } from "./middleware/protectRoute.js"
+
+
 
 
 const app = express();
@@ -20,6 +25,7 @@ app.use(cookieParser()); // allows us to access cookies under the req.
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
 app.use("/api/v1/tv", protectRoute, tvRoutes);
+app.use("/api/v1/search", protectRoute, searchRoutes);
 
 
 
