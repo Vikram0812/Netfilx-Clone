14b0e89ae33e4fd9768b667e153a6f49604f7f05
Vikram0812 <kr.vikram008@gmail.com>
2025-02-21 16:35:25 +0530

    Built AuthScreen UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
 import { Route, Routes } from "react-router-dom"
 import SignupPage from "./pages/SignupPage"
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/home/HomePage"
 import LoginPage from "./pages/LoginPage"
+import Footer from "./components/footer"
 
 function App() {
 
   return (
-    <Routes>
-    <Route path='/' element={<HomePage/>} />
-    <Route path='/login' element={<LoginPage/>} />
-    <Route path='/signup' element={<SignupPage/>} />
-    </Routes>
+    <>
+      <Routes>
+      <Route path='/' element={<HomePage/>} />
+      <Route path='/login' element={<LoginPage/>} />
+      <Route path='/signup' element={<SignupPage/>} />
+      </Routes>
+      <Footer />
+    </>
+    
   )
   
   
